8503cdd61b489105d045fae3529893039d751468
tlkhanh <longkhanhta@gmail.com>
2023-08-05 17:36:33 +0700

    feat: build production

--- apps/shell/src/environments/environment.prod.ts
+++ apps/shell/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true,
+  Profile: 'https://employee-profile.vercel.app',
+  Welfare: 'http://localhost:4201',
+  Uniform: 'http://localhost:4201',
+  Review: 'https://review.vercel.app',
+  Payroll: 'http://localhost:4203',
+  Timesheet: 'http://localhost:4204',
 };
