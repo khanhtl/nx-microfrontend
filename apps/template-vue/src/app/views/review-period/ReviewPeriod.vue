fda060b7a92ac3a237a5e790afd4800ddc20a84f
tlkhanh <longkhanhta@gmail.com>
2023-08-21 23:30:20 +0700

    feat: add api demo

--- apps/template-vue/src/app/views/review-period/ReviewPeriod.vue
+++ apps/template-vue/src/app/views/review-period/ReviewPeriod.vue
@@ -1,44 +1,8 @@
 <template>
-  <div style="margin-top: 150px;"  v-if="isLoading">
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-      style="margin:auto;background:#fff;display:block;" width="200px" height="200px" viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid">
-      <circle cx="84" cy="50" r="10" fill="#e15b64">
-        <animate attributeName="r" repeatCount="indefinite" dur="0.25s" calcMode="spline" keyTimes="0;1" values="10;0"
-          keySplines="0 0.5 0.5 1" begin="0s"></animate>
-        <animate attributeName="fill" repeatCount="indefinite" dur="1s" calcMode="discrete" keyTimes="0;0.25;0.5;0.75;1"
-          values="#e15b64;#abbd81;#f8b26a;#f47e60;#e15b64" begin="0s"></animate>
-      </circle>
-      <circle cx="16" cy="50" r="10" fill="#e15b64">
-        <animate attributeName="r" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="0s"></animate>
-        <animate attributeName="cx" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="0s"></animate>
-      </circle>
-      <circle cx="50" cy="50" r="10" fill="#f47e60">
-        <animate attributeName="r" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.25s"></animate>
-        <animate attributeName="cx" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.25s"></animate>
-      </circle>
-      <circle cx="84" cy="50" r="10" fill="#f8b26a">
-        <animate attributeName="r" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.5s"></animate>
-        <animate attributeName="cx" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.5s"></animate>
-      </circle>
-      <circle cx="16" cy="50" r="10" fill="#abbd81">
-        <animate attributeName="r" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.75s"></animate>
-        <animate attributeName="cx" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.75s"></animate>
-      </circle>
-    </svg>
-  </div>
   <div style="height: calc(100vh - 56px); overflow: auto">
-    <canvas id="line"></canvas>
-    <canvas id="bar"></canvas>
-    <canvas id="doughnut"></canvas>
+    <canvas class="chart" id="line"></canvas>
+    <canvas class="chart" id="bar"></canvas>
+    <canvas class="chart" id="doughnut"></canvas>
   </div>
 </template>
 <script setup lang="ts">
@@ -46,11 +10,8 @@ import { onMounted, ref } from 'vue';
 import { initChartJs } from '../../../../../../libs/core/src/lib/chartjs/init';
 const isLoading=ref(true);
 onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false
-  initChartJs(document.querySelector('#doughnut'), 'doughnut')
-  initChartJs(document.querySelector('#bar'), 'bar')
-  initChartJs(document.querySelector('#line'), 'line')
-  }, 200);
+    initChartJs(document.querySelector('#doughnut'), 'doughnut')
+    initChartJs(document.querySelector('#bar'), 'bar')
+    initChartJs(document.querySelector('#line'), 'line')
 });
 </script>
--- apps/template-vue/src/app/views/targets/Targets.vue
+++ apps/template-vue/src/app/views/targets/Targets.vue
@@ -1,141 +1,19 @@
 <template>
-  <div style="margin-top: 150px" v-if="isLoading">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      style="margin: auto; background: #fff; display: block"
-      width="200px"
-      height="200px"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid"
-    >
-      <circle cx="84" cy="50" r="10" fill="#e15b64">
-        <animate
-          attributeName="r"
-          repeatCount="indefinite"
-          dur="0.25s"
-          calcMode="spline"
-          keyTimes="0;1"
-          values="10;0"
-          keySplines="0 0.5 0.5 1"
-          begin="0s"
-        ></animate>
-        <animate
-          attributeName="fill"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="discrete"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="#e15b64;#abbd81;#f8b26a;#f47e60;#e15b64"
-          begin="0s"
-        ></animate>
-      </circle>
-      <circle cx="16" cy="50" r="10" fill="#e15b64">
-        <animate
-          attributeName="r"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="0s"
-        ></animate>
-        <animate
-          attributeName="cx"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="0s"
-        ></animate>
-      </circle>
-      <circle cx="50" cy="50" r="10" fill="#f47e60">
-        <animate
-          attributeName="r"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.25s"
-        ></animate>
-        <animate
-          attributeName="cx"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.25s"
-        ></animate>
-      </circle>
-      <circle cx="84" cy="50" r="10" fill="#f8b26a">
-        <animate
-          attributeName="r"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.5s"
-        ></animate>
-        <animate
-          attributeName="cx"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.5s"
-        ></animate>
-      </circle>
-      <circle cx="16" cy="50" r="10" fill="#abbd81">
-        <animate
-          attributeName="r"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="0;0;10;10;10"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.75s"
-        ></animate>
-        <animate
-          attributeName="cx"
-          repeatCount="indefinite"
-          dur="1s"
-          calcMode="spline"
-          keyTimes="0;0.25;0.5;0.75;1"
-          values="16;16;16;50;84"
-          keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
-          begin="-0.75s"
-        ></animate>
-      </circle>
-    </svg>
-  </div>
-  <div style="height: calc(100vh - 56px); overflow: auto">
-    <canvas id="doughnut"></canvas>
-    <canvas id="bar"></canvas>
-    <canvas id="line"></canvas>
+  <div class="products gap-8">
+    <div v-for="item in products">
+      <img :src="item.image" alt="" width="200" height="100">
+      <p>{{ item.title }}</p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { initChartJs } from '../../../../../../libs/core/src/lib/chartjs/init';
-const isLoading = ref(true);
-onMounted(() => {
-  setTimeout(() => {
-    isLoading.value = false;
-    initChartJs(document.querySelector('#doughnut'), 'doughnut');
-    initChartJs(document.querySelector('#bar'), 'bar');
-    initChartJs(document.querySelector('#line'), 'line');
-  }, 200);
-});
+import axios from 'axios';
+import { ref } from 'vue';
+async function list() {
+    const response = await axios.get(
+      'https://fakestoreapi.com/products',
+    )
+    return response.data
+  }
+const products = ref(await list())
 </script>
